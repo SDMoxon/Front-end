30b276bc8c41983c156f6f383a200be2c150ebaf
laurajacobs <laurajacobs@talktalk.net>
2017-07-19 16:23:34 +0100

    feat-9 blocks our navbar on rhs (patient page) (#9)

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './component_styles/NavBar.css'
+
+class NavBar extends React.Component {
+    render () {
+        return (
+          <div className="component-NavBar">
+              <ul>  
+              <i className="fa fa-window-close" aria-hidden="true"></i>
+          <i className="fa fa-thermometer-three-quarters" aria-hidden="true"></i>
+          <i className="fa fa-medkit" aria-hidden="true"></i>
+          <i className="fa fa-file-text" aria-hidden="true"></i>
+
+              </ul>
+          </div>
+
+        );
+    }
+}
+
+export default NavBar;
+
+
--- src/pages/PatientPage.js
+++ src/pages/PatientPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import MedicalRecords from '../components/MedicalRecords';
 import PatientSummary from '../components/PatientSummary';
 import VitalsDisplay from '../components/VitalsDisplay';
+import NavBar from '../components/NavBar';
 
 // import './page_styles/LoginPage.css';
 
@@ -12,11 +13,22 @@ class PatientPage extends React.Component {
 
  <div className="columns">
   <div className="column is-4">
-
-
        <MedicalRecords/>
        <PatientSummary/>
        <VitalsDisplay/>
+
+    </div>
+     <div className="column is-4">
+     
+
+    </div>
+     <div className="column is-3">
+     
+
+    </div>
+       <div className="column is-1">
+           <NavBar/>
+     
 
     </div>
 
