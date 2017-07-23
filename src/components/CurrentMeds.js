5b0736e526ab53e7f2db9e85cbe9dced2e4d0cbe
Kavita Varsani <kavi.b0208@gmail.com>
2017-07-23 17:18:30 +0100

    feat-28: styling added to vitals input form (#26)

--- src/components/CurrentMeds.js
+++ src/components/CurrentMeds.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './component_styles/CurrentMeds.css';
+
 class CurrentMeds extends React.Component {
   constructor (props) {
     super(props);
--- src/components/VitalForm.js
+++ src/components/VitalForm.js
@@ -1,65 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './component_styles/VitalForm.css';
 
 class VitalForm extends React.Component {
-	constructor (props) {
+	constructor(props) {
 		super(props);
 	}
-	render () {
+	render() {
 		if (this.props.showVital) {
 			return (
 				<div className="component-vitalForm">
 					<nav className="level">
 						<div className="level-item">
 							<div>
-								<p className="title">Enter Latest Vitals</p>
+								<p id='title' className="title">Enter Latest Vitals</p>
 							</div>
 						</div>
 					</nav>
+					<div className="columns">
 
-					<div className="field">
-						<label className="label">Temperature</label>
-						<div className="control">
-							<input className="input" type="text" placeholder="" />
-						</div>
-					</div>
+						<div className="column">
+							<div className="field">
+								<label className="label">Blood Pressure SYS</label>
+								<div className="control">
+									<input className="input" type="text" placeholder="" />
+								</div>
+							</div>
 
-					<div className="field">
-						<label className="label">Pulse</label>
-						<div className="control">
-							<input className="input" type="email" placeholder="" />
-						</div>
-					</div>
+							<div className="field">
+								<label className="label">Blood Pressure DIA</label>
+								<div className="control">
+									<input className="input" type="email" placeholder="" />
+								</div>
+							</div>
 
-					<div className="field">
-						<label className="label">Blood Pressure SYS</label>
-						<div className="control">
-							<input className="input" type="text" placeholder="" />
+							<div className="field">
+								<label className="label">Respiratory Rate</label>
+								<div className="control">
+									<input className="input" type="email" placeholder="" />
+								</div>
+							</div>
 						</div>
-					</div>
 
-					<div className="field">
-						<label className="label">Blood Pressure DIA</label>
-						<div className="control">
-							<input className="input" type="email" placeholder="" />
-						</div>
-					</div>
+						<div className="column">
+							<div className="field">
+								<label className="label">Temperature</label>
+								<div className="control">
+									<input className="input" type="text" placeholder="" />
+								</div>
+							</div>
 
-					<div className="field">
-						<label className="label">Respiratory Rate</label>
-						<div className="control">
-							<input className="input" type="email" placeholder="" />
-						</div>
-					</div>
+							<div className="field">
+								<label className="label">Pulse</label>
+								<div className="control">
+									<input className="input" type="email" placeholder="" />
+								</div>
+							</div>
 
-					<div className="field">
-						<div className="control">
-							<button className="button is-primary">Confirm & Submit</button>
-							<button
-								className="button is-danger"
-								onClick={this.props.onClose}
-							>Cancel</button>
+							<div className="field">
+								<div className="control">
+									<button className="button is-white">Confirm & Submit</button>
+									<button
+										className="button is-danger is-pulled-right"
+										onClick={this.props.onClose}
+									>Cancel</button>
+								</div>
+
+							</div>
 						</div>
+
+
 					</div>
 				</div>
 			);
